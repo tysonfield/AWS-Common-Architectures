f3be3002cc6d13995687be88b638c6a7736fc80a
tysonfield <tysonfield96@gmail.com>
2022-02-12 15:16:23 +1000

    Added public subnet

--- cdk/cdk_stack.py
+++ cdk/cdk_stack.py
@@ -14,7 +14,12 @@ class CdkStack(Stack):
 			cidr = "10.0.0.0/16",
 			subnet_configuration = [ec2.SubnetConfiguration(
 				name = "test_subnet_private",
-				subnet_type = ec2.SubnetType.PRIVATE_ISOLATED,
+				subnet_type = ec2.SubnetType.PRIVATE_WITH_NAT,
+				cidr_mask = 24
+			),
+			ec2.SubnetConfiguration(
+				name = "test_subnet_public",
+				subnet_type = ec2.SubnetType.PUBLIC,
 				cidr_mask = 24
 			)],
 			max_azs = 1
@@ -29,5 +34,6 @@ class CdkStack(Stack):
 			instance_type = ec2.InstanceType("t3.small"),
 			machine_image = current_ami,
 			vpc = vpc,
-			security_group = security_group
+			security_group = security_group,
+			vpc_subnets = ec2.SubnetSelection(subnets = vpc.select_subnets(subnet_type = ec2.SubnetType.PUBLIC).subnets)
 			)
