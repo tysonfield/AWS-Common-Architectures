8443cabec5b0944fbee146c076e30b53d0c52054
Tyson Field <tysonfield96@gmail.com>
2023-07-17 21:39:06 +1000

    Merge branch 'dev' into 'master' (#3)
    * Renamed cdk_stack.py
    
    * Renamed CdkStack stack
    
    ---------
    
    Co-authored-by: Tyson Field

--- app.py
+++ app.py
@@ -2,11 +2,11 @@
 
 import aws_cdk as cdk
 
-from cdk.cdk_stack import CdkStack
+from cdk.three_tier_simple import ThreeTierSimple
 
 
 app = cdk.App()
 test_env = cdk.Environment(region = "ap-southeast-2")
-CdkStack(app, "cdk", env=test_env)
+ThreeTierSimple(app, "my-app", env=test_env)
 
 app.synth()
